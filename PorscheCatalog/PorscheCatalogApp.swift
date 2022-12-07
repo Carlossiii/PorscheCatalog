0b649e1bcf06c597ed6120eb778437f36d0a5988
Carlossiii <cv5673@gmail.com>
2022-12-07 17:25:04 -0300

    Initial Commit

---
+++ PorscheCatalog/PorscheCatalogApp.swift
@@ -0,0 +1,17 @@
+//
+//  PorscheCatalogApp.swift
+//  PorscheCatalog
+//
+//  Created by Carlos Vinicius on 07/12/22.
+//
+
+import SwiftUI
+
+@main
+struct PorscheCatalogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
