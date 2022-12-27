03afadb7aa1c9a0359e4b2bd3418677ed3a2e670
Carlossiii <cv5673@gmail.com>
2022-12-27 18:46:24 -0300

    Added a vídeo to welcome the user.

--- PorscheCatalog/ContentView.swift
+++ PorscheCatalog/ContentView.swift
@@ -12,18 +12,29 @@ struct ContentView: View {
     var body: some View {
         NavigationStack {
             NavigationLink(destination: ListView(), label: {
-                VStack {
-                    Image("logo")
-                        .resizable()
-                        .scaledToFill()
-                        .frame(width: 300, height: 350)
-                        .padding()
-                    Text("Tap to reveal")
-                        .padding(.vertical)
-                        .padding(.vertical)
-                        .fontDesign(.monospaced)
-                        .font(.title)
-                        .addGlowEffect(color1: Color(Color.RGBColorSpace.sRGB, red: 255/255, green: 50/255, blue: 50/255, opacity: 1), color2: Color(Color.RGBColorSpace.sRGB, red: 238/255, green: 50/255, blue: 50/255, opacity: 1), color3: Color(Color.RGBColorSpace.sRGB, red: 255/255, green: 50/255, blue: 50/255, opacity: 1))
+                ZStack {
+                    
+                    PlayerView()
+                        .edgesIgnoringSafeArea(.all)
+                    
+                    VStack {
+                        
+                        Spacer()
+                        
+                        Image("logo")
+                            .resizable()
+                            .scaledToFill()
+                            .frame(width: 200, height: 250)
+                            .padding()
+                        
+                        Spacer()
+                        
+                        Text("Tap to reveal")
+                            .padding(.vertical)
+                            .fontDesign(.monospaced)
+                            .font(.title)
+                            .addGlowEffect(color1: Color(Color.RGBColorSpace.sRGB, red: 255/255, green: 50/255, blue: 50/255, opacity: 1), color2: Color(Color.RGBColorSpace.sRGB, red: 238/255, green: 50/255, blue: 50/255, opacity: 1), color3: Color(Color.RGBColorSpace.sRGB, red: 255/255, green: 50/255, blue: 50/255, opacity: 1))
+                    }
                 }
             })
             .simultaneousGesture(TapGesture().onEnded {
